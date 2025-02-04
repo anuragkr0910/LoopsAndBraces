20883ef0b4176f3486dc618680543c9581af1ac3
Anurag Kumar <anuragkr0910@gmail.com>
2025-02-04 22:49:55 +0530

    Domain Model created

---
+++ API/LoopsAndBraces.API/LoopsAndBraces.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿namespace LoopsAndBraces.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        public int SerialNo { get; set; }
+
+        public string Title { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Content { get; set; }
+
+        public string FeaturedImageURL { get; set; }
+
+        public string URLHandle { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public string Author { get; set; }
+
+        public bool IsVisible { get; set; }
+    }
+}
---
+++ API/LoopsAndBraces.API/LoopsAndBraces.API/Models/Domain/Category.cs
@@ -0,0 +1,19 @@
+﻿namespace LoopsAndBraces.API.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public int SerialNo { get; set; }
+
+        public string Name { get; set; }
+
+        public string URLHandle { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
